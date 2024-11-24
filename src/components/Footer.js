521ad8d1593b3bcd0e199acb85eece839d808ec2
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2024-11-24 15:35:31 +0100

    add mailto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ export default class Footer extends Component {
         <a href="https://www.linkedin.com/in/agnieszka-skubiszewska-17aaa4106/">LinkedIn</a>
         <a href="https://open.spotify.com/playlist/5NbleROaHyKOZDwJEPm7f5?si=fefe370b536842f0">Space Playlist</a>
         <a href="https://github.com/agnieszkaskubiszewska"> GitHub</a>
+        <a href="mailto:agnieszka.space@wp.pl">Send email</a>
       </div>
     );
   }
